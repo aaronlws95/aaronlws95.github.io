fc234d66b56f9629984e71d3781dd834c8bb677f
aaron <aaronlws95@gmail.com>
2024-04-13 12:52:02 +0800

    Initial filtering

---
+++ generate_project_modals_html.py
@@ -0,0 +1,90 @@
+import yaml
+import re
+from pathlib import Path
+
+modal_html_template = """
+<div id="{id}-modal" class="modal">
+  <div class="modal-container">
+    <div class="modal-content">
+      <h2>{title}</h2>
+      <hr>
+      <img src="{modal_img_url}" alt="{img_alt}">
+      <div class="modal-text">
+        <p>
+        {summary}
+        </p>
+        <p><b>Tags</b>: {tags}</p>
+      </div>
+    </div>
+  </div>
+</div>
+"""
+
+projbox_html_template = """
+  <div class="projbox {tags}">
+    <p class="text">{title}</p>
+    <a href="#{id}-modal"><img src="{projbox_img_url}" alt="{id}"></a>
+  </div>
+"""
+
+filter_button_template = """
+  <div class="filter-button">
+    <button class="active" id="{tag_id}-button" value="{tag_id}" onclick="onFilterButtonPress('{tag_id}-button')">{tag}</button>
+  </div>
+  <br>
+"""
+
+def lower_case_hyphenfy(title: str) -> str:
+    project_id = title.replace(" ", "-")
+    project_id = project_id.lower()
+    project_id = re.sub('[^a-zA-Z0-9\-]', '', project_id).lower()
+    return project_id
+
+if __name__ == "__main__":
+  start_modal_marker = "<!-- Begin Modals -->"
+  end_modal_marker = "<!-- End Modals -->"
+  modals_html_string = f"{start_modal_marker}\n"
+
+  start_projbox_marker = "<!-- Begin Project Boxes -->"
+  end_projbox_marker = "<!-- End Project Boxes -->"
+  projbox_html_string = f'{start_projbox_marker}\n<div class="projbox-container">\n'
+
+  start_filter_button_marker = "<!-- Begin Filter Checkboxes -->"
+  end_filter_button_marker = "<!-- End Filter Checkboxes -->"
+  filter_button_html_string = f'{start_filter_button_marker}\n<div class="sidebar">\n<div class="filter-button-container">\n'
+
+  added_tag = {}
+  for file in Path("projects").glob("*/*.yaml"):
+      with open(file) as f:
+          project_info = yaml.safe_load(f)
+
+      project_id = lower_case_hyphenfy(project_info["title"])
+      tags_comma = ", ".join(project_info["tags"])
+      modal_html = modal_html_template.format(id=project_id, title=project_info["title"], modal_img_url=file.parent/project_info["modal_img_url"], img_alt=project_info["img_alt"], summary=project_info["summary"], tags=tags_comma)
+      modals_html_string += f"{modal_html}\n"
+
+      tag_ids_space = " ".join([lower_case_hyphenfy(tag) for tag in project_info["tags"]])
+      projbox_html = projbox_html_template.format(tags=tag_ids_space, id=project_id, title=project_info["title"], projbox_img_url=file.parent/project_info["projbox_img_url"])
+      projbox_html_string += f"{projbox_html}\n"       
+      
+      for tag in project_info["tags"]:
+        if tag in added_tag:
+          continue
+        added_tag[tag] = True
+        filter_button_html = filter_button_template.format(tag=tag, tag_id=lower_case_hyphenfy(tag))
+        filter_button_html_string += f"{filter_button_html}\n"
+
+  modals_html_string += f"{end_modal_marker}\n"
+  projbox_html_string += f"</div>\n{end_projbox_marker}\n"
+  filter_button_html_string += f"</div>\n</div>\n{end_filter_button_marker}\n"
+
+  # Find the markers in the existing html and replace content
+  with open("projects.html", "r") as f:
+      projects_html_str = f.read()
+      projects_html_str = re.sub(f"{start_modal_marker}(.*?){end_modal_marker}", modals_html_string, projects_html_str, flags=re.DOTALL)
+      projects_html_str = re.sub(f"{start_projbox_marker}(.*?){end_projbox_marker}", projbox_html_string, projects_html_str, flags=re.DOTALL)
+      projects_html_str = re.sub(f"{start_filter_button_marker}(.*?){end_filter_button_marker}", filter_button_html_string, projects_html_str, flags=re.DOTALL)
+
+  with open("projects.html", "w") as f:
+      f.write(projects_html_str)
+      
--- js/modals.js
+++ js/modals.js
@@ -1,53 +1,66 @@
-document.addEventListener('DOMContentLoaded', () => {
-    generateModalTableDisplay();
-});
+filterSelection();
 
 document.body.addEventListener('click', function (evt) {
     onOutsideModalClick(evt);
 }, false);
 
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("collapsed");
+    var sidebarButton = document.getElementById("sidebar-button");
+    sidebarButton.classList.toggle("active");
+}
+
+function onFilterButtonPress(id) {
+    // Set button to active
+    var button = document.getElementById(id);
+
+    button.classList.toggle("active");
+    filterSelection();
+}
+
+function filterSelection() {
+    // Get the selected categories
+    var buttons = document.querySelectorAll(".filter-button-container button");
+    var categories = [];
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].classList.contains("active")) {
+            categories.push(buttons[i].value);
+        }
+    }
+
+    // Apply the filter
+    var items = document.querySelectorAll(".projbox");
+    var show = false;
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        show = false;
+        for (var j = 0; j < categories.length; j++) {
+            if (item.classList.contains(categories[j])) {
+                show = true;
+                item.classList.add("show-projbox");
+                break;
+            }
+        }
+
+        if (!show) {
+            item.classList.remove("show-projbox");
+        }
+    }    
+}
+
+
+
+
 function onOutsideModalClick(evt) {
-    if (evt.target.className !== 'open-modal' && evt.target !== document.body) {
+    if (evt.target.className !== 'modal' && evt.target !== document.body) {
         return;
     }
 
+    // Retain scroll position
     localStorage.setItem('scrollpos', window.scrollY);
     window.location.href = "#";
     var scrollpos = localStorage.getItem('scrollpos');
     if (scrollpos) window.scrollTo(0, scrollpos);
-}
-
-function generateModalTableDisplay() {
-    var contentTable = document.getElementById("content");
-
-    // Remove content if exists
-    while (contentTable.firstChild) {
-        contentTable.removeChild(contentTable.firstChild);
-    }
-
-    for (var i = 0; i < 2; i++) {
-        var projrow = document.createElement("div");
-        projrow.className = "projrow";
-        for (var j = 0; j <= 2; j++) {
-            var proj = document.createElement("div");
-            proj.setAttribute("class", "proj");
-            projrow.appendChild(proj);
-
-            var title = document.createElement("p");
-            title.className = "text";
-            title.innerHTML = "Nature's Turn";
-            proj.appendChild(title);
-
-            var link = document.createElement("a");
-            link.href = "#natures-turn-modal";
-            proj.appendChild(link);
-
-            var img = document.createElement("img");
-            img.src = "img/natures_turn.png";
-            img.alt = "Nature's Turn";
-            link.appendChild(img);
-        }
-        contentTable.appendChild(projrow);
-    }
 }
 
