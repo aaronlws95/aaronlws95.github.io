29ba51dd5d6762add28fe83783324562f09e6aa7
aaron <aaronlws95@gmail.com>
2024-04-14 12:38:08 +0800

    Modal revamp implementation

--- generate_project_modals_html.py
+++ generate_project_modals_html.py
@@ -1,18 +1,21 @@
-import yaml
+import datetime
 import re
 from pathlib import Path
+
+import yaml
 
 modal_html_template = """
 <div id="{id}-modal" class="modal">
   <div class="modal-container">
+    <div class="modal-exit">
+      <a href="#"><img src="img/xbutton.svg" alt="x" width="25px" height="25px"></a>
+    </div>
     <div class="modal-content">
       <h2>{title}</h2>
       <hr>
-      <img src="{modal_img_url}">
+      {modal_imgs}
       <div class="modal-text">
-        <p>
         {summary}
-        </p>
         <p><b>Tags</b>: {tags}</p>
       </div>
     </div>
@@ -21,70 +24,140 @@ modal_html_template = """
 """
 
 projbox_html_template = """
-  <div class="projbox {tags}">
-    <p class="text">{title}</p>
-    <a href="#{id}-modal"><img src="{projbox_img_url}" alt="{id}"></a>
-  </div>
+<div class="projbox {tags}">
+  <p class="text">{title}</p>
+  <a href="#{id}-modal"><img id={id}-projbox-img src="{projbox_img_url}" alt="{id}"></a>
+</div>
 """
 
 filter_button_template = """
-  <div class="tag-button">
-    <button class="active" id="{tag_id}-button" value="{tag_id}" onclick="onFilterButtonPress('{tag_id}-button')">{tag}</button>
-  </div>
-  <br>
+<div class="tag-button">
+  <button class="active" id="{tag_id}-button" value="{tag_id}" onclick="onFilterButtonPress('{tag_id}-button')">{tag}</button>
+</div>
+<br>
 """
+
 
 def lower_case_hyphenfy(title: str) -> str:
     project_id = title.replace(" ", "-")
     project_id = project_id.lower()
-    project_id = re.sub('[^a-zA-Z0-9\-]', '', project_id).lower()
+    project_id = re.sub("[^a-zA-Z0-9\-\#\+]", "", project_id).lower()
     return project_id
 
+
 if __name__ == "__main__":
-  start_modal_marker = "<!-- Begin Modals -->"
-  end_modal_marker = "<!-- End Modals -->"
-  modals_html_string = f"{start_modal_marker}\n"
+    start_modal_marker = "<!-- Begin Modals -->"
+    end_modal_marker = "<!-- End Modals -->"
+    modals_html_string = f"{start_modal_marker}\n"
 
-  start_projbox_marker = "<!-- Begin Project Boxes -->"
-  end_projbox_marker = "<!-- End Project Boxes -->"
-  projbox_html_string = f'{start_projbox_marker}\n<div class="projbox-container">\n'
+    start_projbox_marker = "<!-- Begin Project Boxes -->"
+    end_projbox_marker = "<!-- End Project Boxes -->"
+    projbox_html_string = f'{start_projbox_marker}\n<div class="projbox-container">\n'
 
-  start_filter_button_marker = "<!-- Begin Filter Buttons -->"
-  end_filter_button_marker = "<!-- End Filter Buttons -->"
-  filter_button_html_string = f'{start_filter_button_marker}\n<div id="tags-sidebar">\n<div class="tag-button-container">\n'
+    start_filter_button_marker = "<!-- Begin Filter Buttons -->"
+    end_filter_button_marker = "<!-- End Filter Buttons -->"
+    filter_button_html_string = f'{start_filter_button_marker}\n<div id="tags-sidebar">\n<div class="tag-button-container">\n'
 
-  added_tag = {}
-  for file in Path("projects").glob("*/*.yaml"):
-      with open(file) as f:
-          project_info = yaml.safe_load(f)
+    added_tag = {}
+    projbox_html_data = []
+    for file in Path("projects").glob("*/*.yaml"):
+        with open(file) as f:
+            project_info = yaml.safe_load(f)
 
-      project_id = lower_case_hyphenfy(project_info["title"])
-      tags_comma = ", ".join(project_info["tags"])
-      modal_html = modal_html_template.format(id=project_id, title=project_info["title"], modal_img_url=file.parent/project_info["modal_img_url"], summary=project_info["summary"], tags=tags_comma)
-      modals_html_string += f"{modal_html}\n"
+        project_id = lower_case_hyphenfy(project_info["title"])
+        tags_comma = ", ".join(project_info["tags"])
 
-      tag_ids_space = " ".join([lower_case_hyphenfy(tag) for tag in project_info["tags"]])
-      projbox_html = projbox_html_template.format(tags=tag_ids_space, id=project_id, title=project_info["title"], projbox_img_url=file.parent/project_info["projbox_img_url"])
-      projbox_html_string += f"{projbox_html}\n"       
-      
-      for tag in project_info["tags"]:
-        if tag in added_tag:
-          continue
-        added_tag[tag] = True
-        filter_button_html = filter_button_template.format(tag=tag, tag_id=lower_case_hyphenfy(tag))
-        filter_button_html_string += f"{filter_button_html}\n"
+        modal_imgs = ""
+        for img_url in project_info["modal_img_url"]:
+            modal_imgs += (
+                f'<img class={project_id}-modal-img src="{file.parent}/{img_url}">\n'
+            )
 
-  modals_html_string += f"{end_modal_marker}\n"
-  projbox_html_string += f"</div>\n{end_projbox_marker}\n"
-  filter_button_html_string += f"</div>\n</div>\n{end_filter_button_marker}\n"
+        modal_html = modal_html_template.format(
+            id=project_id,
+            title=project_info["title"],
+            modal_imgs=modal_imgs,
+            summary=project_info["summary"],
+            tags=tags_comma,
+        )
+        modals_html_string += f"{modal_html}"
 
-  # Find the markers in the existing html and replace content
-  with open("projects.html", "r") as f:
-      projects_html_str = f.read()
-      projects_html_str = re.sub(f"{start_modal_marker}(.*?){end_modal_marker}", modals_html_string, projects_html_str, flags=re.DOTALL)
-      projects_html_str = re.sub(f"{start_projbox_marker}(.*?){end_projbox_marker}", projbox_html_string, projects_html_str, flags=re.DOTALL)
-      projects_html_str = re.sub(f"{start_filter_button_marker}(.*?){end_filter_button_marker}", filter_button_html_string, projects_html_str, flags=re.DOTALL)
+        tag_ids_space = " ".join(
+            [lower_case_hyphenfy(tag) for tag in project_info["tags"]]
+        )
+        date = project_info["date"]
+        projbox_html_data.append(
+            (
+                date,
+                projbox_html_template.format(
+                    tags=tag_ids_space,
+                    id=project_id,
+                    title=project_info["title"],
+                    projbox_img_url=file.parent / project_info["projbox_img_url"],
+                ),
+            )
+        )
 
-  with open("projects.html", "w") as f:
-      f.write(projects_html_str)
-      
+        for tag in project_info["tags"]:
+            if tag in added_tag:
+                continue
+            added_tag[tag] = True
+
+    this_website_proj_box_html = """
+<div class="projbox webdev">
+    <a href="index.html">
+    <div id="web-proj">
+        <p>This Website</p>
+    </div>
+    </a>
+</div>
+    """
+    projbox_html_data.append(
+        (datetime.date(2019, 1, 1), this_website_proj_box_html)
+    )
+
+    # List project boxes from most recent
+    sorted_projbox_html_data = [
+        projbox_html
+        for _, projbox_html in sorted(
+            projbox_html_data, reverse=True, key=lambda item: item[0]
+        )
+    ]
+    for projbox_html in sorted_projbox_html_data:
+        projbox_html_string += f"{projbox_html}"
+
+    # List tags alphabetically
+    for tag in sorted(added_tag.keys()):
+        filter_button_html = filter_button_template.format(
+            tag=tag, tag_id=lower_case_hyphenfy(tag)
+        )
+        filter_button_html_string += f"{filter_button_html}"
+
+    modals_html_string += f"{end_modal_marker}"
+    projbox_html_string += f"</div>\n{end_projbox_marker}"
+    filter_button_html_string += f"</div>\n</div>\n{end_filter_button_marker}"
+
+    # Find the markers in the existing html and replace content
+    with open("projects.html", "r") as f:
+        projects_html_str = f.read()
+        projects_html_str = re.sub(
+            f"{start_modal_marker}(.*?){end_modal_marker}",
+            modals_html_string,
+            projects_html_str,
+            flags=re.DOTALL,
+        )
+        projects_html_str = re.sub(
+            f"{start_projbox_marker}(.*?){end_projbox_marker}",
+            projbox_html_string,
+            projects_html_str,
+            flags=re.DOTALL,
+        )
+        projects_html_str = re.sub(
+            f"{start_filter_button_marker}(.*?){end_filter_button_marker}",
+            filter_button_html_string,
+            projects_html_str,
+            flags=re.DOTALL,
+        )
+
+    with open("projects.html", "w") as f:
+        f.write(projects_html_str)
--- js/modals.js
+++ js/modals.js
@@ -1,20 +1,46 @@
 filterSelection();
 
-document.body.addEventListener('click', function (evt) {
-    onOutsideModalClick(evt);
-}, false);
+document.body.addEventListener(
+    "click",
+    function (evt) {
+        onOutsideModalClick(evt);
+    },
+    false,
+);
 
+document.body.addEventListener("keydown", function (e) {
+    if (e.key == "Escape") {
+        exitModal();
+    }
+});
 function toggleSidebar() {
-    var sidebar = document.getElementById("tags-sidebar");
-    sidebar.classList.toggle("collapsed");
-    var sidebarButton = document.getElementById("sidebar-toggle-button");
-    sidebarButton.classList.toggle("active");
+    var buttons = document.querySelectorAll(".tag-button-container button");
+    var activeCount = 0;
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].classList.contains("active")) {
+            activeCount += 1;
+        }
+    }
+
+    var setActive = false;
+    if (activeCount < buttons.length / 2) {
+        setActive = true;
+    }
+
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].classList.contains("active") && !setActive) {
+            buttons[i].classList.remove("active");
+        } else if (!buttons[i].classList.contains("active") && setActive) {
+            buttons[i].classList.add("active");
+        }
+    }
+
+    filterSelection();
 }
 
 function onFilterButtonPress(id) {
     // Set button to active
     var button = document.getElementById(id);
-
     button.classList.toggle("active");
     filterSelection();
 }
@@ -32,6 +58,7 @@ function filterSelection() {
     // Apply the filter
     var items = document.querySelectorAll(".projbox");
     var show = false;
+    var numShow = 0;
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
         show = false;
@@ -46,18 +73,33 @@ function filterSelection() {
         if (!show) {
             item.classList.remove("show-projbox");
         }
-    }    
+
+        if (item.classList.contains("show-projbox")) {
+            numShow += 1;
+        }
+    }
+
+    var gif = document.getElementById("no-project-gif");
+    if (numShow === 0) {
+        gif.classList.add("show");
+    }
+    else if (gif.classList.contains("show")) {
+        gif.classList.remove("show");
+    }
 }
 
 function onOutsideModalClick(evt) {
-    if (evt.target.className !== 'modal' && evt.target !== document.body) {
+    if (evt.target.className !== "modal" && evt.target !== document.body) {
         return;
     }
 
-    // Retain scroll position
-    localStorage.setItem('scrollpos', window.scrollY);
-    window.location.href = "#";
-    var scrollpos = localStorage.getItem('scrollpos');
-    if (scrollpos) window.scrollTo(0, scrollpos);
+    exitModal();
 }
 
+function exitModal() {
+    // Retain scroll position
+    localStorage.setItem("scrollpos", window.scrollY);
+    window.location.href = "#";
+    var scrollpos = localStorage.getItem("scrollpos");
+    if (scrollpos) window.scrollTo(0, scrollpos);
+}
